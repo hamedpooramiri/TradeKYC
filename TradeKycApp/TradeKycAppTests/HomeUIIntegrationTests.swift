96e3516060df1de7bec3961bb198ec6c98b1ca3c
hamed <hamedpooramiri1007@gmail.com>
2024-01-06 20:59:19 +0330

    feat: create file  HomeUIIntegrationTests

--- TradeKycApp/TradeKycAppTests/HomeUIIntegrationTests.swift
+++ TradeKycApp/TradeKycAppTests/HomeUIIntegrationTests.swift
@@ -1,14 +1,13 @@
 //
-//  TradeKycAppTests.swift
+//  HomeUIIntegrationTests.swift
 //  TradeKycAppTests
 //
 //  Created by hamedpouramiri on 1/6/24.
 //
 
 import XCTest
-@testable import TradeKycApp
 
-final class TradeKycAppTests: XCTestCase {
+final class HomeUIIntegrationTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
